ddb526483c1c1ce3e7b68d2ca4539d047aef44fb
Inon Baguio <inon.baguio@yahoo.com>
2017-09-18 08:22:49 +0000

    Added CsvParser to take care of the parsing of fridge items

---
+++ src/Utilities/CsvParser.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Inon\Utilities;
+
+/**
+ * The CsvParser class.
+ *
+ * @package Inon\Utilities\CsvParser
+ * @author  Inon Baguio <inon@vroomvroomvroom.com.au>
+ */
+class CsvParser
+{
+    /**
+     * @var string
+     */
+    private $fileName;
+
+    /**
+     * CsvParser constructor.
+     *
+     * @param string $fileName
+     */
+    public function __construct(string $fileName)
+    {
+        $this->fileName = $fileName;
+
+    }
+
+    /**
+     * @return array
+     */
+    public function parse() : array
+    {
+        $row = 1;
+
+        if (($handle = fopen($this->fileName, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+
+                if ($row <= 1) {
+                    $row++;
+                    continue;
+                }
+
+                $items[$data[0]] = [
+                    'amount' => $data[1],
+                    'unit' => $data[2],
+                    'useby' => $data[3]
+                ];
+            }
+            fclose($handle);
+        }
+
+        return $items
+    }
+}
