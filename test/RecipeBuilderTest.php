ee8d093a05b72a9839d8f17fbe5fbde2dec49f7d
Inon Baguio <inon.baguio@yahoo.com>
2017-09-18 09:45:43 +0000

    Added testGetRecipe and testCheckFoundRecipe

--- test/RecipeBuilderTest.php
+++ test/RecipeBuilderTest.php
@@ -2,13 +2,83 @@
 
 namespace Inon\Test;
 
+use Inon\Console\RecipeBuilder;
+use Inon\Utilities\CsvParser;
+
 /**
  * The RecipeBuilderTest class.
  *
  * @package Inon\Test\RecipeBuilderTest
  * @author  Inon Baguio <inon.baguio@yahoo.com>
  */
-class RecipeBuilderTest
+class RecipeBuilderTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var RecipeBuilder
+     */
+    private $recipeBuilder;
 
+    /**
+     *
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $fridgeItems = (new CsvParser(__DIR__ . '/stubs/fridge.csv'))->parse();
+
+        $this->recipeBuilder = new RecipeBuilder($fridgeItems, $this->getSampleRecipes());
+    }
+
+    public function testGetRecipe()
+    {
+        $this->assertEquals('saladsandwich', $this->recipeBuilder->getRecipe());
+    }
+
+    public function testCheckFoundRecipe()
+    {
+        $recipes = $this->recipeBuilder->checkFoundRecipes();
+
+        $this->assertCount(2, $recipes);
+        $this->assertArrayHasKey('name', $recipes[0]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getSampleRecipes() : array
+    {
+        return [
+            [
+                'name' => 'grilledcheeseontoast',
+                'ingredients' => [
+                    [
+                        'item' => 'bread',
+                        'amount' => 2,
+                        'unit' => 'slices'
+                    ],
+                    [
+                        'item' => 'cheese',
+                        'amount' => 2,
+                        'unit' => 'slices'
+                    ],
+                ],
+            ],
+            [
+                'name' => 'saladsandwich',
+                'ingredients' => [
+                    [
+                        'item' => 'bread',
+                        'amount' => 2,
+                        'unit' => 'slices'
+                    ],
+                    [
+                        'item' => 'mixed salad',
+                        'amount' => 2,
+                        'unit' => 'grams'
+                    ],
+                ],
+            ]
+        ];
+    }
 }
